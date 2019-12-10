72d21c361683bdc7e41e65a95b573ea886e71061
Roman Ershov <>
2019-12-10 21:55:57 +0300

    Moved templates which Travis doesn't need to .cpp files

--- include/task2.h
+++ include/task2.h
@@ -1,9 +1,6 @@
 #ifndef TASK2_H
 #define TASK2_H
 
-#include <cstdlib>
-#include <iostream>
-
 template<class T, unsigned N>
 T* createArr(T(*gen)()) {
 	auto arr = new T[N];
@@ -11,32 +8,6 @@ T* createArr(T(*gen)()) {
 		arr[i] = gen();
 	}
 	return arr;
-}
-
-template<class T>
-T genForTest() {
-	return std::rand() % 256;
-}
-template<>
-char* genForTest() {
-	unsigned N = std::rand() % 11 + 2;
-	char* str = new char[N];
-	for (unsigned i = 0; i < N - 1; i++) {
-		str[i] = std::rand() % 26 + 'a';
-	}
-	str[N - 1] = 0;
-	return str;
-}
-
-template<class T>
-void printArr(T* arr, unsigned N) {
-	for (unsigned i = 0; i < N; i++) {
-		std::cout << arr[i];
-		if (i < N - 1) {
-			std::cout << ", ";
-		}
-	}
-	std::cout << std::endl;
 }
 
 #endif
--- src/main1.cpp
+++ src/main1.cpp
@@ -1,5 +1,33 @@
 #include "task1.h"
 #include "task2.h"
+#include <iostream>
+#include <cstdlib>
+
+template<class T>
+void printArr(T* arr, unsigned N) {
+	for (unsigned i = 0; i < N; i++) {
+		std::cout << arr[i];
+		if (i < N - 1) {
+			std::cout << ", ";
+		}
+	}
+	std::cout << std::endl;
+}
+
+template<class T>
+T genForTest() {
+	return std::rand() % 256;
+}
+template<>
+char* genForTest() {
+	unsigned N = std::rand() % 11 + 2;
+	char* str = new char[N];
+	for (unsigned i = 0; i < N - 1; i++) {
+		str[i] = std::rand() % 26 + 'a';
+	}
+	str[N - 1] = 0;
+	return str;
+}
 
 int main() {
 	const unsigned n1 = 27, n2 = 14;
